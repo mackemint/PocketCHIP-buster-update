6e61f0a9a14c5b0a6cd99397b44635192a668bcd
djdiskmachine <djdiskmachine@pm.me>
2021-03-25 13:00:13 +0000

    Why not save old things just for old times' sake?

--- scripts/update_stretch.sh
+++ scripts/update_stretch.sh
@@ -1,9 +1,14 @@
+#Save old stuff, feeling nostalgic
 cp /etc/apt/sources.list /etc/apt/sources.list~jessie
+cp /etc/NetworkManager/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf~jessie
+cp /etc/X11/xorg.conf /etc/X11/xorg.conf~jessie
+cp /home/chip/.config/awesome/rc.lua /home/chip/.config/awesome/rc.lua~jessie
+#Update to new stuff
 cp ../assets/sources_stretch.list  /etc/apt/sources.list
 cp ../assets/NetworkManager.conf  /etc/NetworkManager/NetworkManager.conf
-
+#Perform upgrade
 sudo apt update --allow-insecure-repositories --allow-unauthenticated && sudo apt upgrade -y --force-yes --allow-unauthenticated && sudo apt dist-upgrade -y --force-yes --allow-unauthenticated
-
+#
 cp ../assets/xorg.conf_debian9  /etc/X11/xorg.conf
 cp ../assets/rc.lua  /home/chip/.config/awesome/rc.lua
 
