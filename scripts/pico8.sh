441cc473b84d0c857729fa92c04f72c2f1d20f4a
djdiskmachine <djdiskmachine@pm.me>
2021-03-28 07:59:02 +0000

    ubihealthd.service nice 19

--- scripts/pico8.sh
+++ scripts/pico8.sh
@@ -1,4 +1,4 @@
-my_process="env LD_PRELOAD=/usr/lib/pico-8/libcurl.so.3 /usr/lib/pico-8/pico8" # check with htop that this is where the process runs from, might be usr/lib/pico8
+my_process="env LD_PRELOAD=/usr/lib/pico-8/libcurl.so.3 /usr/lib/pico-8/pico8 -splore" # check with htop that this is where the process runs from, might be usr/lib/pico8
 #Make CPU hogs more polite
 sudo renice 17 -p $(pidof /lib/systemd/systemd) 
 sudo renice 17 -p $(pidof /lib/systemd/ubihealthd) 
